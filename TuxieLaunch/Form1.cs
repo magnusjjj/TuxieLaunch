963fca125b4d40fab5018edee1763c05fb9e5733
Magnus Johnsson <magnusjjj@gmail.com>
2018-10-21 19:31:48 +0200

    Fixed handling of if the file already exists

--- TuxieLaunch/Form1.cs
+++ TuxieLaunch/Form1.cs
@@ -212,7 +212,14 @@ namespace TuxieLaunch
 
             foreach (string file_name in System.IO.Directory.GetFiles(source_dir, "*", System.IO.SearchOption.AllDirectories))
             {
-                System.IO.File.Copy(file_name, System.IO.Path.Combine(destination_dir, file_name.Substring(source_dir.Length)));
+                try
+                {
+                    System.IO.File.Copy(file_name, System.IO.Path.Combine(destination_dir, file_name.Substring(source_dir.Length)));
+                } catch
+                {
+
+                }
+                
             }
 
 
